fe55a15c4a36d448ed64a8b242e2cfe8dd71dd50
Corujaoprogramador <deivid9pereira@gmail.com>
2025-05-08 08:45:03 -0300

    adicionando sql inicial

---
+++ sql/micro-saas.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE micro_saas_agendamentos;
+USE micro_saas_agendamentos;
+
+CREATE TABLE Cliente (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(100) NOT NULL,
+  cpf VARCHAR(14) NOT NULL UNIQUE,
+  endereco TEXT,
+  contato VARCHAR(100),
+  email VARCHAR(100) NOT NULL UNIQUE,
+  senha_hash VARCHAR(255) NOT NULL,
+  cidade_id INT NOT NULL,
+  FOREIGN KEY (cidade_id) REFERENCES Cidade(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Prestador (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome_fantasia VARCHAR(100),
+  nome_completo VARCHAR(100) NOT NULL,
+  foto_perfil VARCHAR(255),
+  especialidade VARCHAR(100),
+  endereco TEXT,
+  descricao TEXT,
+  data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  senha_hash VARCHAR(255) NOT NULL,
+  cidade_id INT NOT NULL,
+  FOREIGN KEY (cidade_id) REFERENCES Cidade(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Cidade (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100)
+);
+
+CREATE TABLE ImagemServico (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  prestador_id INT NOT NULL,
+  caminho_imagem VARCHAR(255) NOT NULL,
+  descricao TEXT,
+  FOREIGN KEY (prestador_id) REFERENCES Prestador(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Disponibilidade (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  prestador_id INT NOT NULL,
+  dia_semana ENUM('SEG','TER','QUA','QUI','SEX','SAB','DOM') NOT NULL,
+  hora_inicio TIME NOT NULL,
+  hora_fim TIME NOT NULL,
+  FOREIGN KEY (prestador_id) REFERENCES Prestador(id)
+    ON DELETE CASCADE
+);
+
+-- agendamento em si
+CREATE TABLE Agendamento (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  cliente_id INT NOT NULL,
+  prestador_id INT NOT NULL,
+  data_agendamento DATE NOT NULL,
+  hora_inicio TIME NOT NULL,
+  hora_fim TIME NOT NULL,
+  status ENUM('solicitado', 'aceito', 'concluido') DEFAULT 'solicitado',
+  criado_em DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (cliente_id) REFERENCES Cliente(id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (prestador_id) REFERENCES Prestador(id)
+    ON DELETE CASCADE,
+  UNIQUE (prestador_id, data_agendamento, hora_inicio, hora_fim)
+);
+
+-- historico do agendamento
+CREATE TABLE LogAgendamento (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  agendamento_id INT NOT NULL,
+  status ENUM('solicitado', 'aceito', 'concluido') NOT NULL,
+  data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (agendamento_id) REFERENCES Agendamento(id)
+    ON DELETE CASCADE
+);
+
+INSERT INTO Cidade (nome) VALUES ('Araraquara'), ('Matão'), ('Jaboticabal');
